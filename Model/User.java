7e07b53140d715ca1d9f381afe9b6098a426f6e7
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-05-07 21:02:03 +0530

    Create User.java

---
+++ Model/User.java
@@ -0,0 +1,27 @@
+package com.Shrishti.EcommerceApplication.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "userTabl")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer userId;
+    private String userName;
+
+    @Column(name = "userEmail")
+    private String email;
+    private String password;
+
+    @Column(name="userPhone")
+    private String phone;
+}
