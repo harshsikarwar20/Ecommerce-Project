4cec28cc6291e76118490081faf414deed887274
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-05-07 21:02:39 +0530

    Create Product.java

---
+++ Model/Product.java
@@ -0,0 +1,27 @@
+package com.Shrishti.EcommerceApplication.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="productTabl")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id")
+    private Integer productId;
+    private String productName;
+
+    @Column(name="productPrice")
+    private Integer price;
+
+    private String description;
+    private String category;
+    private String brand;
+}
